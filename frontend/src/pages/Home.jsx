84d352967d0c3bb655d91625988c431ab02cdc5e
Nikolaos Papadopoulos <72943192+ngUnick@users.noreply.github.com>
2024-05-08 20:33:29 +0300

    Create background and title

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "../App.css";
+import Background from "./Background";
+import Title from "./Title";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <Background></Background>
+        <Title></Title>
+        <div className="main">dgfgfd</div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
